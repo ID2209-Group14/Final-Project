60cca71081682a9ec656101af297cffdf76e35e8
BingxueXu <autoxue@gmail.com>
2023-12-15 15:12:58 +0100

    randomize the price

--- dutchAuctionWorking.py
+++ dutchAuctionWorking.py
@@ -1,8 +1,9 @@
 import autogen
+import random
 
 
 def conduct_auction(auctioneer, bidders, starting_price, decrement, minimum_price):
-    winningBidder = bidders[0];
+    winningBidder = bidders[0]
     current_price = starting_price
     for bidder in bidders: 
         auctioneer.send(recipient=bidder, message="Are you ready to start the auction?")
@@ -25,7 +26,7 @@ def conduct_auction(auctioneer, bidders, starting_price, decrement, minimum_pric
             bidder.send(message=reply, recipient=auctioneer)
             if "I accept" in reply:
                 any_bid_accepted = True
-                winningBidder = bidder;
+                winningBidder = bidder
                 break
 
         if any_bid_accepted:
@@ -47,12 +48,12 @@ def main():
     config_list = [
         {"model": "palm/chat-bison", "base_url": "http://localhost:8000", "api_key": "NULL"}
     ]
-    llm_config = {"timeout": 600, "config_list": config_list, "temperature": 1}
+    llm_config = {"timeout": 600, "config_list": config_list, "temperature": 0.5}
 
-    starting_price = 1500
+    starting_price = random.randint(2000, 3000)
     
-    decrement = 100
-    minimum_price = 1400
+    decrement = random.randint(100,500)
+    minimum_price = random.randint(1000, 2000)
 
     auctioneer = autogen.ConversableAgent(
             "auctioneer",
@@ -66,7 +67,8 @@ def main():
     bidders = [
         autogen.ConversableAgent(f"bidder{i}", llm_config=llm_config, max_consecutive_auto_reply=10,
             human_input_mode="NEVER", system_message=f"You are a bidder in a Dutch auction. Your budget is ${budget} dollars. The price will be accepted only if it is equal or less than your budget. In that case you will reply 'I accept the price'.")
-        for i, budget in enumerate([900, 1100, 1300, 400])
+        for i in range(4)
+        for budget in [random.randint(1000, 2000)]
     ]
 
 
