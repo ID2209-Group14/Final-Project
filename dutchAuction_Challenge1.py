4132c834c98909c0634a0f0ab2c134e248595ce8
abhi8080 <99359469+abhi8080@users.noreply.github.com>
2023-12-17 19:51:54 +0100

    remove unneccesary files and rename files

--- dutchAuction_Challenge1.py
+++ dutchAuction_Challenge1.py
@@ -30,7 +30,7 @@ def conduct_auction(auctioneer, bidders, bidder_interests, genre, starting_price
         for bidder in bidders:
             if bidder_interests[bidder]:
                 auctioneer.send(recipient=bidder, message=f"Selling {genre} for ${current_price}.") 
-                time.sleep(0.5)
+                time.sleep(1)
         
         any_bid_accepted = False
         for bidder in bidders:
@@ -38,7 +38,6 @@ def conduct_auction(auctioneer, bidders, bidder_interests, genre, starting_price
                 message = bidder.last_message(auctioneer)
                 reply = bidder.generate_reply(messages=[message], sender=auctioneer)
                 bidder.send(message=reply, recipient=auctioneer)
-                time.sleep(0.5)
                 if "I accept" in reply:
                     any_bid_accepted = True
                     winningBidder = bidder
@@ -46,7 +45,6 @@ def conduct_auction(auctioneer, bidders, bidder_interests, genre, starting_price
 
         if any_bid_accepted:
             auctioneer.send(message="Congratulations, you won the auction. Please provide some feedback on how you experienced the auction.", recipient=winningBidder, request_reply=True)
-            time.sleep(0.5)
             return
         else:
             current_price -= decrement
@@ -56,7 +54,6 @@ def conduct_auction(auctioneer, bidders, bidder_interests, genre, starting_price
     for bidder in bidders:
         if bidder_interests[bidder]:
             auctioneer.send(message=reply, recipient=bidder)
-            time.sleep(0.5)
 
      
 
@@ -64,13 +61,15 @@ def main():
     config_list = [
         {"model": "palm/chat-bison", "base_url": "http://localhost:8000", "api_key": "NULL"}
     ]
-    llm_config = {"timeout": 600, "config_list": config_list, "temperature": 0.5}
+    llm_config = {"timeout": 600, "config_list": config_list, "temperature": 0.6}
 
-    starting_price = random.randint(2000, 3000)
-    decrement = random.randint(500,1000)
-    minimum_price = random.randint(1000, 2000)
+    starting_prices = [random.randint(2000, 3000), random.randint(2000, 3000), random.randint(2000, 3000)]
+    decrements = [random.randint(100,500), random.randint(100, 500), random.randint(100,500)]
+    minimum_prices = [random.randint(100, 1000), random.randint(100, 1000), random.randint(100, 1000)]
+
 
     genres = ["CDs", "Clothes", "Arts"]
+
 
     auctioneers = [
         autogen.ConversableAgent(
@@ -78,30 +77,33 @@ def main():
             max_consecutive_auto_reply=10,
             human_input_mode="NEVER",
             llm_config=llm_config,
-            system_message=f"You are an auctioneer in a Dutch auction. You are selling {genre}, You starting price is ${starting_price} dollars, and you will decrease the price by ${decrement} dollars in each round.")
+            system_message=f"You are an auctioneer in a Dutch auction. You are selling {genre}, You starting price is ${starting_prices[i]} dollars, and you will decrease the price by ${decrements[i]} dollars in each round.")
             for i, genre in enumerate(genres)
     ]
     
 
     bidders = [
         autogen.ConversableAgent(f"bidder{i}", llm_config=llm_config, max_consecutive_auto_reply=10,
-            human_input_mode="NEVER", system_message=f"You are a bidder in a Dutch auction. Your genre is {genre}, and you must participate in an auction and it can only be in your genre auction. When the auctioneer asks if you are ready, and the auctioneer is selling {genre}, then you must reply with 'Yes, I am ready'. Your budget is ${budget} dollars. The price will be accepted only if it is equal or less than your budget. In that case you will reply 'I accept the price'.")
-        for i in range(6)
-        for budget in [random.randint(1000, 2000)]
+            human_input_mode="NEVER", system_message=f"You are a bidder in a Dutch auction. Your genre is {genre}, and you must participate in an auction and it can only be in your genre auction. When the auctioneer asks if you are ready and the auctioneer is selling {genre}, then you must reply with 'Yes, I am ready'. Your budget is ${budget} dollars. During the auction, the price will be accepted only if it is equal or less than your budget. In that case you will reply 'I accept the price'.")
+        for i in range(8)
+        for budget in [random.randint(100, 2000)]
         for genre in [random.choice(genres)]
     ]
 
     threads = []
 
+    i = 0
+
     for auctioneer in auctioneers:
         genre = auctioneer.system_message.split("You are selling ")[1].split(",")[0]
         bidder_interests = bidderInterest(auctioneer, genre, bidders)
-        starting_price = random.randint(2000, 3000)
-        decrement = random.randint(500, 1000)
-        minimum_price = random.randint(1000, 2000)
+        starting_price = starting_prices[i]
+        decrement = decrements[i]
+        minimum_price = minimum_prices[i]
 
         auction_thread = threading.Thread(target=conduct_auction, args=(auctioneer, bidders, bidder_interests, genre, starting_price, decrement, minimum_price))
         threads.append(auction_thread)
+        i += 1
 
     
     for thread in threads:
